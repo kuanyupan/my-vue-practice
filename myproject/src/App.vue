e073aa0eaf016054816c96bc56dd5d162eb36b0d
Kuanyu <kuanyu861225@gmail.com>
2020-02-12 19:59:27 +0800

    my first vue practice commit

---
+++ myproject/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+    <Navbar />
+    <nav>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Contact</a></li>
+        </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+import Navbar from './Navbar'
+export default {
+  name: 'app',
+  components: {
+    Navbar
+  },
+  data () {
+    return {
+      title: 'My first Vue app~~'
+    }
+  }
+}
+</script>
+
+<style>
+h1{
+  color: #444;
+  font-weight: normal;
+  text-align: center;
+}
+</style>
